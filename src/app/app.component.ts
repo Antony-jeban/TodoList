456b783238bbc9bb53efcf0de2ed6b86b340d4b7
Jeban, Antony <antony.jeban@hrblock.com>
2019-11-21 15:04:34 +0530

    Added a proper navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    public navbarOpen: boolean;
+
+    toggleNavbar() {
+        this.navbarOpen = !this.navbarOpen;
+    }
 
 }
